3253f56bc9967551d6f0f929b4fb167555ae1b3c
Lidia Popdimitrova <104692293+lidiapopdimitrova@users.noreply.github.com>
2023-04-08 22:50:14 +0300

    Add files via upload

---
+++ project/movie_world.py
@@ -0,0 +1,21 @@
+from project.customer import Customer
+
+
+class MovieWorld:
+    def __init__(self, name):
+        self.name = name
+        self.customers = []
+        self.dvds = []
+
+    @staticmethod
+    def dvd_capacity():
+        return 15
+
+    @staticmethod
+    def customer_capacity():
+        return 10
+
+    def add_customer(self, customer: Customer):
+        if len(self.customers) == self.customer_capacity():
+            pass
+
