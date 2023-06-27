862d7d2d1cd28ace16c48edb423b8da4582ac33a
Lidia Popdimitrova <104692293+lidiapopdimitrova@users.noreply.github.com>
2023-06-27 12:23:54 +0300

    Delete departments_app directory

--- departments_app/departments_app/departments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from departments_app.departments.views import show_departments, show_department_details
-
-urlpatterns = (
-    path('', show_departments),
-    path('<department_id>/', show_department_details),
-    path('int/<int:department_id>/', show_department_details),
-)
-
